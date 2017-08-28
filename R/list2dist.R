7272bcdc1fd0266409d384b5898f5885ee549eb6
Andrew B. Collier <andrew@exegetic.biz>
2017-08-28 10:56:40 +0200

    More efficient implementation.

--- R/list2dist.R
+++ R/list2dist.R
@@ -1,26 +1,38 @@
+#' Convert data frame to distance matrix.
+#'
+#' @param dat A data frame with (at least) three columns.
+#'
+#' @return A distance matrix.
+#' @export
+#'
+#' @examples
 list2dist <-
-function(dat){
-    dat.name1 <- as.character(dat[,1])
-    dat.name2 <- as.character(dat[,2])
-    dat.value <- dat[,3]
-    names1 <- sort(unique(as.character(dat[,1])))
-    names2 <- sort(unique(as.character(dat[,2])))
-    total.names <- unique(c(names1, names2))
-    elements <- rep(NA, length(total.names)^2)
-    dim(elements) <- c(length(total.names),length(total.names))
-    rownames(elements) <- total.names
-    colnames(elements) <- total.names
-    
-    for(i in 1:length(total.names)){
-         for(j in 1:length(total.names)){
-              for(k in 1:length(dat.name1)){
-                  if((total.names[i] == dat.name1[k])&(total.names[j] == dat.name2[k])){
-                      elements[i,j] <- dat.value[k]
-                    }
-              }
-         }
-    }
-    res <- as.dist(t(elements))
-    return(res)
+function (dat) 
+{
+  dat.name1 <- as.character(dat[, 1])
+  dat.name2 <- as.character(dat[, 2])
+  dat.value <- dat[, 3]
+  #
+  total.names = union(dat.name1, dat.name2)
+  #
+  elements <- matrix(nrow = length(total.names), ncol = length(total.names))
+  #
+  # Set diagonal elements to zero.
+  #
+  diag(elements) <- 0
+  #
+  rownames(elements) <- total.names
+  colnames(elements) <- total.names
+  #
+  rows = factor(dat.name1, levels = total.names)
+  cols = factor(dat.name2, levels = total.names)
+  #
+  for (n in 1:nrow(dat)) {
+    irow = as.integer(rows[n])
+    jcol = as.integer(cols[n])
+    elements[irow, jcol] = dat.value[n]
+  }
+  print(elements[1:5,1:5])
+  #
+  as.dist(t(elements), upper = TRUE)
 }
-
