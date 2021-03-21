3d901e210110e0cdeeeba2db8d90fc4240e5766b
Jinlong Zhang <jinlongzhang01@gmail.com>
2021-03-21 15:41:15 +0800

    rewritten of sp.pair, returning lower matrices

--- R/sp.pair.R
+++ R/sp.pair.R
@@ -1,77 +1,95 @@
-sp.pair <-
-function(matr)
-{
-   if(any(is.na(matr))){
-      matr <- na.omit(matr)
-      cat("The data matrix contains NA, and have been removed.\n")
-   }
-   pearson  <- cor(matr, method = "pearson")
-   spearman <- cor(matr, method = "spearman")
-   matr1 = matr
-   N <- nrow(matr1) ##number of plots
-   matr1[matr1>1] <- 1
-   n <- apply(matr1, 2, sum) ##number of species for each plots
-
-   tmatr <- t(matr1)
-   df <- as.matrix(tmatr)
-
-   a <- df %*% t(df)          ## Number of plots that both A and B are present
-   b <- df %*% (1 - t(df))    ## Number of plots only A is present
-   c <- (1 - df) %*% t(df)    ## Number of plots Presence for speciesB
-   d <- ncol(df) - a - b - c  ## Number of plots absence for both speciesA and  speciesA
-   ##Yate's correction for chisq
-   chisq <- data.frame()
-   V <- data.frame()
-   Ochiai <- data.frame()
-   Dice <- data.frame()
-   Jaccard <- data.frame()
-   PCC <- data.frame()
-   for (i in 1:nrow(a)){
-       for (j in 1:ncol(a)){
-           chisq[i,j]   <- ((((abs(a[i,j] * d[i,j] - b[i,j]*c[i,j])
-                           - 0.5*N)^2)*N )/((a[i,j]+b[i,j])*(a[i,j]+c[i,j])*(b[i,j]
-                           +d[i,j])*(c[i,j]+d[i,j])))
-           V[i,j]       <- ((a[i,j]+d[i,j])-(b[i,j]+c[i,j]))/(a[i,j]+b[i,j]+c[i,j]+d[i,j]) # V>0,positive, V<0,negative # Thanks for Ms Xueni Zhang for pointing out the error.
-           Ochiai[i,j]  <- a[i,j]/sqrt((a[i,j]+b[i,j])*(a[i,j]+c[i,j])) #Ochiai index
-           Dice[i,j]    <- 2*a[i,j] /(2*a[i,j] + b[i,j] + c[i,j]) #Dice index
-           Jaccard[i,j] <- a[i,j]/(a[i,j]+b[i,j]+c[i,j]) #Jaccard index
-           PCC[i,j]     <- (a[i,j] * d[i,j] - b[i,j] * c[i,j])/((a[i,j] + b[i,j])*(a[i,j]
-                            + c[i,j])*(c[i,j] + d[i,j])*(b[i,j] + d[i,j])) ##Percentage cooccurance
-       }
-    }
-
-   chisq <- data.frame(chisq)
-   dd <- data.frame()
-   for (i in 1:nrow(chisq)){
-       for (j in 1:ncol(chisq)){
-            if (chisq[i, j] > 6.635){
-                chisq[i, j] <- paste(chisq[i, j], "**")
-                }
-            if (chisq[i, j] < 6.635& chisq[i, j] > 3.841){
-                chisq[i, j] <- paste(chisq[i, j], "*")
-                }
-        dd[i,j] <- a[i,j] * d[i,j] - b[i,j] * c[i,j]
-        }
-   }
-   AC = data.frame() ## Association Coefficients
-   for (i in 1: nrow(a)){
-       for (j in 1: ncol(a)){
-           if (a[i,j] * d[i,j] >= b[i,j] * c[i,j]){
-               AC[i,j] <- (a[i,j] * d[i,j] - b[i,j] * c[i,j])/
-                          ((a[i,j] + b[i,j])*(b[i,j] + d[i,j]))
+sp.pair <- function(matr){
+      if (any(is.na(matr))) {
+         matr <- na.omit(matr)
+         cat("Rows containing NA have been removed.\n")
+      }
+      
+      pearson  <- cor(matr, method = "pearson")
+      spearman <- cor(matr, method = "spearman")
+      
+      N <- nrow(matr)     # number of plots
+      matr[matr > 1] <- 1 # presence-absence matrix
+      
+      empty_mat <- matrix(data = rep(NA, ncol(matr)*ncol(matr)), 
+                          nrow = ncol(matr), 
+                          ncol = ncol(matr), 
+                          dimnames = list(colnames(matr), colnames(matr)))
+      colnames(empty_mat) <- colnames(matr)
+      rownames(empty_mat) <- colnames(matr)    
+      
+      ##Yate's correction for chisq
+      chisq   <- empty_mat
+      V       <- empty_mat
+      Ochiai  <- empty_mat
+      Dice    <- empty_mat
+      Jaccard <- empty_mat
+      PCC     <- empty_mat
+      dd      <- empty_mat
+      AC      <- empty_mat
+      
+      for (i in 1:ncol(matr)) {
+         if(i < ncol(matr)){
+            for (j in (i+1):ncol(matr)){
+               temp_occurrence_sp1 <- matr[,i]
+               temp_occurrence_sp2 <- matr[,j]
+               temp_contigent_tab  <- table(temp_occurrence_sp1, temp_occurrence_sp2)
+               
+               a <- temp_contigent_tab[2,2] # number of plots in which both sp1 and sp2 are present
+               b <- temp_contigent_tab[1,2] # number of plots in which sp1 is present, sp2 is absent
+               c <- temp_contigent_tab[2,1] # number of plots in which sp1 is absent, sp2 is present
+               d <- temp_contigent_tab[1,1] # number of plots in which both sp1 and sp2 are absent
+               
+               # Thanks for Ms Xueni Zhang for pointing out the error
+               ### j is the row. i is the column
+               ### always use the lower matrix
+               ### j is dependent to i, j is always greater than i
+               
+               # V>0,positive, V<0,negative 
+               V[j,i]       <-  ((a+d)-(b+c))/(a+b+c +d)
+               Ochiai[j,i]  <-  a/sqrt((a+b)*(a+c))      #Ochiai index
+               Dice[j,i]    <-  2*a/(2*a+b+c)            #Dice index
+               Jaccard[j,i] <-  a/(a+b+c)                #Jaccard index
+               PCC[j,i]     <-  (a*d-b*c)/((a+b)*(a+c)*(c+d)*(b+d)) ##Percentage cooccurance
+               dd[j,i]      <-  a*d-b*c
+               
+               # Chi square
+               chisq[j,i] <- ((((abs(a*d-b*c)-0.5*N) ^ 2)*N)/
+                                  ((a+b)*(a+c)*(b+d)*(c+d)))
+               
+               # the Association index AC
+               # -Wang Bosun, Peng Shaolin. Studies on the Measuring Techniques 
+               # of Interspecific Association of Lower-Subtropical Evergreen-
+               # Broadleaved Forests. I. The Exploration and the Revision 
+               # on the Measuring Formulas of Interspecific Association[J]. 
+               # Chin J Plan Ecolo, 1985, 9(4): 274-279.
+               # -Hurlbert, S. H. (1969). A coefficient of interspecific association. 
+               # Ecology, 50(1), 1-9.
+               
+               if (a*d >= b*c) {
+                  AC[j,i] <- (a*d-b*c) /((a+b)*(b+d))
+               } else { # a*d < b*c
+                  if( a <= d ){
+                     AC[j,i] <- (a*d-b*c) /((a+b)*(a+c))
+                  } else { # a > d
+                     AC[j,i] <- (a*d-b*c) /((b+d)*(c+d))
+                  }
+                  
                }
-           if ( a[i,j]*d[i,j] < b[i,j]*c[i,j] & d[i,j] >= a[i,j]){
-              AC[i,j] <- (a[i,j] * d[i,j] - b[i,j] * c[i,j])/
-                         ((a[i,j] + b[i,j])*(a[i,j] + c[i,j]))
-              }
-
-           if (b[i,j]*c[i,j] > a[i,j] * d[i,j] & d[i,j] < a[i,j] ){
-              AC[i,j] <- (a[i,j] * d[i,j] - b[i,j] * c[i,j])/
-                         ((b[i,j] + d[i,j])*(d[i,j] + c[i,j]))}
-       }
-   }
-   result <- list(chisq = chisq, chisqass = dd, V = V, Ochiai = Ochiai,
-                  Dice = Dice, Jaccard = Jaccard, Pearson = pearson,
-                  Spearman = spearman, PCC = PCC, AC = AC )
-   return(result)
+            }
+         }
+      }
+      result <-
+         list(
+            chisq    = as.dist(chisq),
+            chisqass = as.dist(dd),
+            V        = as.dist(V),
+            Ochiai   = as.dist(Ochiai),
+            Dice     = as.dist(Dice),
+            Jaccard  = as.dist(Jaccard),
+            Pearson  = pearson,
+            Spearman = spearman,
+            PCC      = as.dist(PCC),
+            AC       = as.dist(AC)
+         )
+      return(result)
 }
