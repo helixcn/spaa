83ca99e292bad1fea423365313b10b8566d7faa3
Jinlong Zhang <jinlongzhang01@gmail.com>
2021-08-12 11:42:24 +0800

    reformatted using styler

--- R/plotnetwork.R
+++ R/plotnetwork.R
@@ -1,148 +1,163 @@
 plotnetwork <-
-function(datainput,                    # The correlation matrix, usually a lower matrix
-         n_levels = 6,                 # Number of types of segments to show the correlation, note interval should <12
-         xlim = c(-2.5, 5),            # horizontal range of the canvas
-         ylim = c(-2.5, 2.5),          # vertical range of the canvas
-         node_size = 3,                # size of label
-         lwd.var = TRUE,               # logical, if the segments width should vary with the absolute value
-         lwd = 4,                      # width of the segments for connecting the sites, default 1
-         label_dist = 1.2,             # Distance of text labels from each corner.
-         show.node = TRUE,             # Whether the nodes in the figure should be labeled
-         show.text.label = TRUE,       # Whether the text label should be drawn.
-         linecol = c("orange", "blue"),# Colours showing positve or negative correlation.
-         # The lines representing positive correlations use the first element
-         show.legend = TRUE,           # If the legend should be shown
-         valuename = "r",              # Name of the variable shown in the legend
-         legendx = 3,                  # the starting position of the legend x
-         legendy = -2,                 # the starting position of the legend y
-         legend_line_space = 1,        # the space between the lines in the legend
-         legend_linelength = 0.3,      # length of the segment in the legend
-         adjust_legend_x = 0,          # adjusting the position of legend (x)
-         adjust_legend_y = 0,          # adjusting the position of legend (y)
-         digits = 2,                   # Number of digits shown in the legend, 
-         # it will be used in generating the breaks as well
-         ...                           # other parameters related with plot.default
-        ){
-
+  function(datainput, # The correlation matrix, usually a lower matrix
+           n_levels = 6, # Number of types of segments to show the correlation, note interval should <12
+           xlim = c(-2.5, 5), # horizontal range of the canvas
+           ylim = c(-2.5, 2.5), # vertical range of the canvas
+           node_size = 3, # size of label
+           lwd.var = TRUE, # logical, if the segments width should vary with the absolute value
+           lwd = 4, # width of the segments for connecting the sites, default 1
+           label_dist = 1.2, # Distance of text labels from each corner.
+           show.node = TRUE, # Whether the nodes in the figure should be labeled
+           show.text.label = TRUE, # Whether the text label should be drawn.
+           linecol = c("orange", "blue"), # Colours showing positve or negative correlation.
+           # The lines representing positive correlations use the first element
+           show.legend = TRUE, # If the legend should be shown
+           valuename = "r", # Name of the variable shown in the legend
+           legendx = 3, # the starting position of the legend x
+           legendy = -2, # the starting position of the legend y
+           legend_line_space = 1, # the space between the lines in the legend
+           legend_linelength = 0.3, # length of the segment in the legend
+           adjust_legend_x = 0, # adjusting the position of legend (x)
+           adjust_legend_y = 0, # adjusting the position of legend (y)
+           digits = 2, # Number of digits shown in the legend,
+           # it will be used in generating the breaks as well
+           ... # other parameters related with plot.default
+  ) {
     datainput <- as.matrix(datainput)
 
-    if(any(is.na(datainput))){
-        stop("NA is not allowed")
+    if (any(is.na(datainput))) {
+      stop("NA is not allowed")
     }
-    if(n_levels > 6|n_levels < 2){
-        stop("Number of levels should be between 2 and 6")
+    if (n_levels > 6 | n_levels < 2) {
+      stop("Number of levels should be between 2 and 6")
     }
-    if (!nrow(datainput)==ncol(datainput)){
-        stop("The input is not a correlative matrix.")
+    if (!nrow(datainput) == ncol(datainput)) {
+      stop("The input is not a correlative matrix.")
     }
-    npoints = nrow(datainput)
-    if (npoints > 10){
-        warning("Too many sites to show! Please use matrix with fewer (e.g., <10) dimensions")
+    npoints <- nrow(datainput)
+    if (npoints > 10) {
+      warning("Too many sites to show! Please use matrix with fewer (e.g., <10) dimensions")
     }
 
-    plot(0,0, xlim = xlim, ylim = ylim,
-         type = "n", axes = FALSE, xlab="", ylab="", ...)
+    plot(0, 0,
+      xlim = xlim, ylim = ylim,
+      type = "n", axes = FALSE, xlab = "", ylab = "", ...
+    )
 
-    seqn  <- (seq(0, n_levels^2, by = n_levels))/(n_levels^2)
-    limit0 <- sort(c(min(datainput) + (max(datainput)-min(datainput))*seqn))
+    seqn <- (seq(0, n_levels^2, by = n_levels)) / (n_levels^2)
+    limit0 <- sort(c(min(datainput) + (max(datainput) - min(datainput)) * seqn))
 
-    subangle <- 2*pi/npoints
+    subangle <- 2 * pi / npoints
 
-    r=2
+    r <- 2
 
     xdep <- c()
     ydep <- c()
     txdep <- c()
     tydep <- c()
 
-    for (k in 1:npoints){
-        r = r
-        x = r*sin(subangle * k)
-        y = r*cos(subangle * k)
-        tx = label_dist*x
-        ty = label_dist*y
-        xdep <- append(xdep, x)
-        ydep <- append(ydep, y)
-        txdep <- append(txdep, tx)   # Position for labels x
-        tydep <- append(tydep, ty)   # Position for labels y
+    for (k in 1:npoints) {
+      r <- r
+      x <- r * sin(subangle * k)
+      y <- r * cos(subangle * k)
+      tx <- label_dist * x
+      ty <- label_dist * y
+      xdep <- append(xdep, x)
+      ydep <- append(ydep, y)
+      txdep <- append(txdep, tx) # Position for labels x
+      tydep <- append(tydep, ty) # Position for labels y
     }
 
     ltys <- sort(1:n_levels, decreasing = TRUE)
 
-    for (i in 1:length(xdep)){
-        for (j in 1:i){ # Only loop through the lower matrix
-            for (m in 1:n_levels){
-                colori <- ifelse(datainput[i,j] > 0, linecol[1], linecol[2])
-                    if (datainput[i,j] >= limit0[m] & datainput[i,j] < limit0[m + 1]){
-                        segments(x0 = xdep[i],
-                                 y0 = ydep[i],
-                                 x1 = xdep[j],
-                                 y1 = ydep[j],
-                                 lty = ltys[m],  # m is the variable telling the what kind of lines to draw
-                                 col= colori,
-                                 lwd = ifelse(lwd.var, abs(mean(limit0[m:(m+1)]))*lwd, lwd))
-                 } else {
-                 if(datainput[i,j] == limit0[m + 1]){
-                         segments(x0 = xdep[i],
-                                 y0 = ydep[i],
-                                 x1 = xdep[j],
-                                 y1 = ydep[j],
-                                 lty = ltys[m],  # m is the variable telling the what kind of lines to draw
-                                 col= colori,
-                                 lwd = ifelse(lwd.var, abs(mean(limit0[m:(m+1)]))*lwd, lwd))
-                    }
-                }
-            }
-        }
-    }
-
-   if(show.node){
-       points(xdep, ydep, cex=node_size, pch = 21, bg = "white", lwd = lwd/2)
-       text(xdep, ydep, 1:npoints, cex = node_size/4)
-   }
-   if(show.text.label){
-       text(txdep, tydep, colnames(datainput))
-   }
-   if(show.legend){
-       for (n in 1:n_levels){
-            col_n <- ifelse(mean(limit0[n] + limit0[n+1]) > 0, linecol[1], linecol[2])
-
-            segments(legendx + adjust_legend_x,
-                     legendy + legend_line_space * n * 0.25 + adjust_legend_y,
-                     legendx + adjust_legend_x + legend_linelength,
-                     legendy + legend_line_space * n * 0.25 + adjust_legend_y,
-                     lty = ltys[n],
-                     #col = col_n,
-                     lwd = ifelse(lwd.var,
-                                  ifelse(abs(mean(limit0[n:(n+1)]))*lwd < 0.01,
-                                              0.1,
-                                              abs(mean(limit0[n:(n+1)]))*lwd),
-                                              lwd)
-                     )
-            if(n < length(limit0)){
-
-                    text(legendx + adjust_legend_x + 5/4 + 0.5 + legend_linelength, 
-                     legendy + legend_line_space*n*0.25 + adjust_legend_y, 
-                     paste(formatC(sprintf("%.2f", limit0[n+1]), width = 5)))
-
-                    text(legendx + adjust_legend_x + 4/4 + 0.5 + legend_linelength, 
-                     legendy + legend_line_space*n*0.25 + adjust_legend_y, 
-                     expression(""<""))
-
-                    text(legendx + adjust_legend_x + 3/4 + 0.5 + legend_linelength, 
-                     legendy + legend_line_space*n*0.25 + adjust_legend_y, 
-                     valuename)
-
-
-                    text(legendx + adjust_legend_x + 2/4 + 0.5 + legend_linelength, 
-                    legendy + legend_line_space*n*0.25 + adjust_legend_y, 
-                    expression(""<=""))
-
-                    text(legendx + adjust_legend_x + 1/4 + 0.5 + legend_linelength, 
-                    legendy + legend_line_space*n*0.25 + adjust_legend_y, 
-                    paste(formatC(sprintf("%.2f", limit0[n]), width = 5)))
-
+    for (i in 1:length(xdep)) {
+      for (j in 1:i) { # Only loop through the lower matrix
+        for (m in 1:n_levels) {
+          colori <- ifelse(datainput[i, j] > 0, linecol[1], linecol[2])
+          if (datainput[i, j] >= limit0[m] & datainput[i, j] < limit0[m + 1]) {
+            segments(
+              x0 = xdep[i],
+              y0 = ydep[i],
+              x1 = xdep[j],
+              y1 = ydep[j],
+              lty = ltys[m], # m is the variable telling the what kind of lines to draw
+              col = colori,
+              lwd = ifelse(lwd.var, abs(mean(limit0[m:(m + 1)])) * lwd, lwd)
+            )
+          } else {
+            if (datainput[i, j] == limit0[m + 1]) {
+              segments(
+                x0 = xdep[i],
+                y0 = ydep[i],
+                x1 = xdep[j],
+                y1 = ydep[j],
+                lty = ltys[m], # m is the variable telling the what kind of lines to draw
+                col = colori,
+                lwd = ifelse(lwd.var, abs(mean(limit0[m:(m + 1)])) * lwd, lwd)
+              )
             }
           }
+        }
+      }
     }
-}
+
+    if (show.node) {
+      points(xdep, ydep, cex = node_size, pch = 21, bg = "white", lwd = lwd / 2)
+      text(xdep, ydep, 1:npoints, cex = node_size / 4)
+    }
+    if (show.text.label) {
+      text(txdep, tydep, colnames(datainput))
+    }
+    if (show.legend) {
+      for (n in 1:n_levels) {
+        col_n <- ifelse(mean(limit0[n] + limit0[n + 1]) > 0, linecol[1], linecol[2])
+
+        segments(legendx + adjust_legend_x,
+          legendy + legend_line_space * n * 0.25 + adjust_legend_y,
+          legendx + adjust_legend_x + legend_linelength,
+          legendy + legend_line_space * n * 0.25 + adjust_legend_y,
+          lty = ltys[n],
+          # col = col_n,
+          lwd = ifelse(lwd.var,
+            ifelse(abs(mean(limit0[n:(n + 1)])) * lwd < 0.01,
+              0.1,
+              abs(mean(limit0[n:(n + 1)])) * lwd
+            ),
+            lwd
+          )
+        )
+        if (n < length(limit0)) {
+          text(
+            legendx + adjust_legend_x + 5 / 4 + 0.5 + legend_linelength,
+            legendy + legend_line_space * n * 0.25 + adjust_legend_y,
+            paste(formatC(sprintf("%.2f", limit0[n + 1]), width = 5))
+          )
+
+          text(
+            legendx + adjust_legend_x + 4 / 4 + 0.5 + legend_linelength,
+            legendy + legend_line_space * n * 0.25 + adjust_legend_y,
+            expression("" < "")
+          )
+
+          text(
+            legendx + adjust_legend_x + 3 / 4 + 0.5 + legend_linelength,
+            legendy + legend_line_space * n * 0.25 + adjust_legend_y,
+            valuename
+          )
+
+
+          text(
+            legendx + adjust_legend_x + 2 / 4 + 0.5 + legend_linelength,
+            legendy + legend_line_space * n * 0.25 + adjust_legend_y,
+            expression("" <= "")
+          )
+
+          text(
+            legendx + adjust_legend_x + 1 / 4 + 0.5 + legend_linelength,
+            legendy + legend_line_space * n * 0.25 + adjust_legend_y,
+            paste(formatC(sprintf("%.2f", limit0[n]), width = 5))
+          )
+        }
+      }
+    }
+  }
--- R/sp.pair.R
+++ R/sp.pair.R
@@ -1,129 +1,129 @@
-sp.pair <- function(matr){
-   
-   format_contigent_tab <- function(aaa){
-      temp_mat <- rep(0, 4) 
-      dim(temp_mat) <- c(2, 2)
-      
-      tab_row_name <- rownames(aaa)
-      tab_col_name <- colnames(aaa)
-      
-      for (i in 1:nrow(aaa)){
-         for (j in 1:ncol(aaa)){
-            if(tab_row_name[i] == "0" & tab_col_name[j] == "0"){
-               temp_mat[1,1] <- aaa[i, j]
-            }
-            
-            if(tab_row_name[i] == "0" & tab_col_name[j] == "1"){
-               temp_mat[1,2] <- aaa[i, j]
-            }
-            
-            if(tab_row_name[i] == "1" & tab_col_name[j] == "0"){
-               temp_mat[2,1] <- aaa[i, j]
-            }
-            
-            if(tab_row_name[i] == "1" & tab_col_name[j] == "1"){
-               temp_mat[2,2] <- aaa[i, j]
-            }
-            
-         }
+sp.pair <- function(matr) {
+  format_contigent_tab <- function(aaa) {
+    temp_mat <- rep(0, 4)
+    dim(temp_mat) <- c(2, 2)
+
+    tab_row_name <- rownames(aaa)
+    tab_col_name <- colnames(aaa)
+
+    for (i in 1:nrow(aaa)) {
+      for (j in 1:ncol(aaa)) {
+        if (tab_row_name[i] == "0" & tab_col_name[j] == "0") {
+          temp_mat[1, 1] <- aaa[i, j]
+        }
+
+        if (tab_row_name[i] == "0" & tab_col_name[j] == "1") {
+          temp_mat[1, 2] <- aaa[i, j]
+        }
+
+        if (tab_row_name[i] == "1" & tab_col_name[j] == "0") {
+          temp_mat[2, 1] <- aaa[i, j]
+        }
+
+        if (tab_row_name[i] == "1" & tab_col_name[j] == "1") {
+          temp_mat[2, 2] <- aaa[i, j]
+        }
       }
-      
-      rownames(temp_mat) <- c("sp1_absent", "sp1_present")
-      colnames(temp_mat) <- c("sp2_absent", "sp2_present")
-      
-      return(temp_mat)
-   }
-   
-      if (any(is.na(matr))) {
-         matr <- na.omit(matr)
-         cat("Rows containing NA have been removed.\n")
+    }
+
+    rownames(temp_mat) <- c("sp1_absent", "sp1_present")
+    colnames(temp_mat) <- c("sp2_absent", "sp2_present")
+
+    return(temp_mat)
+  }
+
+  if (any(is.na(matr))) {
+    matr <- na.omit(matr)
+    cat("Rows containing NA have been removed.\n")
+  }
+
+  pearson <- cor(matr, method = "pearson")
+  spearman <- cor(matr, method = "spearman")
+
+  N <- nrow(matr) # number of plots
+  matr[matr > 1] <- 1 # presence-absence matrix
+
+  empty_mat <- matrix(
+    data = rep(NA, ncol(matr) * ncol(matr)),
+    nrow = ncol(matr),
+    ncol = ncol(matr),
+    dimnames = list(colnames(matr), colnames(matr))
+  )
+  colnames(empty_mat) <- colnames(matr)
+  rownames(empty_mat) <- colnames(matr)
+
+  ## Yate's correction for chisq
+  chisq <- empty_mat
+  V <- empty_mat
+  Ochiai <- empty_mat
+  Dice <- empty_mat
+  Jaccard <- empty_mat
+  PCC <- empty_mat
+  dd <- empty_mat
+  AC <- empty_mat
+
+  for (i in 1:ncol(matr)) {
+    if (i < ncol(matr)) {
+      for (j in (i + 1):ncol(matr)) {
+        sp1 <- matr[, i]
+        sp2 <- matr[, j]
+
+        temp_contigent_tab <- format_contigent_tab(table(sp1, sp2))
+
+        a <- temp_contigent_tab[2, 2] # number of plots in which both sp1 and sp2 are present
+        b <- temp_contigent_tab[2, 1] # number of plots in which sp1 is present, sp2 is absent
+        c <- temp_contigent_tab[1, 2] # number of plots in which sp1 is absent, sp2 is present
+        d <- temp_contigent_tab[1, 1] # number of plots in which both sp1 and sp2 are absent
+
+        # Thanks for Ms Xueni Zhang for pointing out the error
+        ### j is the row. i is the column
+        ### always use the lower matrix
+        ### j is dependent to i, j is always greater than i
+
+        # V>0,positive, V<0,negative
+        V[j, i] <- ((a + d) - (b + c)) / (a + b + c + d)
+        Ochiai[j, i] <- a / sqrt((a + b) * (a + c)) # Ochiai index
+        Dice[j, i] <- 2 * a / (2 * a + b + c) # Dice index
+        Jaccard[j, i] <- a / (a + b + c) # Jaccard index
+        PCC[j, i] <- (a * d - b * c) / ((a + b) * (a + c) * (c + d) * (b + d)) ## Percentage cooccurance
+        dd[j, i] <- a * d - b * c
+
+        # Chi square
+        chisq[j, i] <- ((((abs(a * d - b * c) - 0.5 * N)^2) * N) /
+          ((a + b) * (a + c) * (b + d) * (c + d)))
+
+        # the Association index AC
+        # -Wang Bosun, Peng Shaolin. Studies on the Measuring Techniques
+        # of Interspecific Association of Lower-Subtropical Evergreen-
+        # Broadleaved Forests. I. The Exploration and the Revision
+        # on the Measuring Formulas of Interspecific Association[J].
+        # Chin J Plan Ecolo, 1985, 9(4): 274-279.
+        # -Hurlbert, S. H. (1969). A coefficient of interspecific association.
+        # Ecology, 50(1), 1-9.
+
+        if (a * d >= b * c) {
+          AC[j, i] <- (a * d - b * c) / ((a + b) * (b + d))
+        } else { # a*d < b*c
+          if (a <= d) {
+            AC[j, i] <- (a * d - b * c) / ((a + b) * (a + c))
+          } else { # a > d
+            AC[j, i] <- (a * d - b * c) / ((b + d) * (c + d))
+          }
+        }
       }
-
-      pearson  <- cor(matr, method = "pearson")
-      spearman <- cor(matr, method = "spearman")
-
-      N <- nrow(matr)     # number of plots
-      matr[matr > 1] <- 1 # presence-absence matrix
-
-      empty_mat <- matrix(data = rep(NA, ncol(matr)*ncol(matr)),
-                           nrow = ncol(matr),
-                           ncol = ncol(matr),
-                           dimnames = list(colnames(matr), colnames(matr)))
-      colnames(empty_mat) <- colnames(matr)
-      rownames(empty_mat) <- colnames(matr)
-
-      ##Yate's correction for chisq
-      chisq   <- empty_mat
-      V       <- empty_mat
-      Ochiai  <- empty_mat
-      Dice    <- empty_mat
-      Jaccard <- empty_mat
-      PCC     <- empty_mat
-      dd      <- empty_mat
-      AC      <- empty_mat
-
-      for (i in 1:ncol(matr)) {
-         if(i < ncol(matr)){
-            for (j in (i+1):ncol(matr)){
-               sp1 <- matr[,i]
-               sp2 <- matr[,j]
-               
-               temp_contigent_tab <- format_contigent_tab(table(sp1,sp2))
-
-               a <- temp_contigent_tab[2,2] # number of plots in which both sp1 and sp2 are present
-               b <- temp_contigent_tab[2,1] # number of plots in which sp1 is present, sp2 is absent
-               c <- temp_contigent_tab[1,2] # number of plots in which sp1 is absent, sp2 is present
-               d <- temp_contigent_tab[1,1] # number of plots in which both sp1 and sp2 are absent
-
-               # Thanks for Ms Xueni Zhang for pointing out the error
-               ### j is the row. i is the column
-               ### always use the lower matrix
-               ### j is dependent to i, j is always greater than i
-
-               # V>0,positive, V<0,negative
-               V[j,i]       <-  ((a+d)-(b+c))/(a+b+c +d)
-               Ochiai[j,i]  <-  a/sqrt((a+b)*(a+c))      #Ochiai index
-               Dice[j,i]    <-  2*a/(2*a+b+c)            #Dice index
-               Jaccard[j,i] <-  a/(a+b+c)                #Jaccard index
-               PCC[j,i]     <-  (a*d-b*c)/((a+b)*(a+c)*(c+d)*(b+d)) ##Percentage cooccurance
-               dd[j,i]      <-  a*d-b*c
-
-               # Chi square
-               chisq[j,i] <- ((((abs(a*d-b*c)-0.5*N) ^ 2)*N)/
-                                    ((a+b)*(a+c)*(b+d)*(c+d)))
-
-               # the Association index AC
-               # -Wang Bosun, Peng Shaolin. Studies on the Measuring Techniques
-               # of Interspecific Association of Lower-Subtropical Evergreen-
-               # Broadleaved Forests. I. The Exploration and the Revision
-               # on the Measuring Formulas of Interspecific Association[J].
-               # Chin J Plan Ecolo, 1985, 9(4): 274-279.
-               # -Hurlbert, S. H. (1969). A coefficient of interspecific association.
-               # Ecology, 50(1), 1-9.
-
-               if (a*d >= b*c) {
-                  AC[j,i] <- (a*d-b*c) /((a+b)*(b+d))
-               } else { # a*d < b*c
-                  if( a <= d ){
-                     AC[j,i] <- (a*d-b*c) /((a+b)*(a+c))
-                  } else { # a > d
-                     AC[j,i] <- (a*d-b*c) /((b+d)*(c+d))
-                  }
-               }
-            }
-         }
-      }
-      result <-
-         list(
-            chisq    = as.dist(chisq),
-            V        = as.dist(V),
-            Ochiai   = as.dist(Ochiai),
-            Dice     = as.dist(Dice),
-            Jaccard  = as.dist(Jaccard),
-            Pearson  = pearson,
-            Spearman = spearman,
-            PCC      = as.dist(PCC),
-            AC       = as.dist(AC)
-         )
-      return(result)
+    }
+  }
+  result <-
+    list(
+      chisq    = as.dist(chisq),
+      V        = as.dist(V),
+      Ochiai   = as.dist(Ochiai),
+      Dice     = as.dist(Dice),
+      Jaccard  = as.dist(Jaccard),
+      Pearson  = pearson,
+      Spearman = spearman,
+      PCC      = as.dist(PCC),
+      AC       = as.dist(AC)
+    )
+  return(result)
 }
